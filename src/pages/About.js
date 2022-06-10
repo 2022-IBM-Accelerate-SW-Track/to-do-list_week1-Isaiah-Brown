f599ecde9f3bea1f77f4a50b1ed31659e2f466b9
Isaiah Brown <isaiahbrown@Isaiahs-MacBook-Pro.local>
2022-06-09 21:18:01 -0400

    first main branch commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import about_me_pic from "../assets/about_me_pic.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+             /*  src="../assets/about_me_pic.jpeg" */
+              about_me_pic
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Isaiah Brown</div>
+            <div className="brief_description">
+              Salutations! 
+              I'm a rising junior at Franklin & Marshall College.
+              Awhile back, my hamster "joe" died. Very sad!  
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
